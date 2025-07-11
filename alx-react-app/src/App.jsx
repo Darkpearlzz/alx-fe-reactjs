245ea1950ce0b3b26efa22cd8ed819c1845339bc
Rhoda Ugboro <rhodaugboro@gmail.com>
2025-07-11 16:50:55 +0100

    Add new files

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
-import Header from "./Header";
-import MainContent from "./MainContent";
-import Footer from "./Footer";
+import WelcomeMessage from "./components/WelcomeMessage";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import Header from "./components/header";
 
 function App() {
   return (
     <>
+      <WelcomeMessage />
+
       <Header />
 
       <MainContent />
