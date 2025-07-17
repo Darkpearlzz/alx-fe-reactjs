ac5dbf18e33a8474caff1a0cb69a2006934e3b95
Rhoda Ugboro <rhodaugboro@gmail.com>
2025-07-17 17:16:50 +0100

    Add new file

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import WelcomeMessage from "./components/WelcomeMessage";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import Header from "./components/header";
+import UserProfile from "./components/UserProfile";
+function App() {
+  return (
+    <>
+      <WelcomeMessage />
+
+      <Header />
+
+      <MainContent />
+
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+  return (
+    <main>
+      <p style={{ fontStyle: "italic", color: "coral", fontSize: "20px" }}>
+        I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
