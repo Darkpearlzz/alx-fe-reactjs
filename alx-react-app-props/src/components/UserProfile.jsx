93b173ec024f8986ab0d68c9c256052408e193f6
Rhoda Ugboro <rhodaugboro@gmail.com>
2025-07-18 23:46:26 +0100

    Made changes to file

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,7 +1,11 @@
+import { useContext } from "react";
 import UserInfo from "./components/UserInfo";
+import UserContext from "./components/UserContext";
 
 function UserProfile() {
-  return <UserInfo />;
+  const userData = useContext(UserContext);
+
+  return <UserInfo userData={userData} />;
 }
 
 export default UserProfile;
