3714da244f311b46eb90faee518fbb0a7217d782
Rhoda Ugboro <rhodaugboro@gmail.com>
2025-08-07 08:39:27 +0100

    Updated Responsive Styling

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="user-profile p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-10 md:my-20 bg-gray-100 rounded-lg shadow-lg">
+    <div className="user-profile sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-10 md:my-20 bg-gray-100 rounded-lg shadow-lg">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
