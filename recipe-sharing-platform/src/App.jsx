10af6dc05e2c153410749fea77dd84a735ce769e
Rhoda <rhodaugboro@gmail.com>
2025-08-15 01:09:05 +0100

    Recipe sharing platform

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-6">
+      <h1 className="text-blue-500 text-3xl font-bold">
+        Tailwind is working!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
